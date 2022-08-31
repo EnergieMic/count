4a6db95fccd48bc3d90b49dc5c3b4188283f0a38
brouilletm <brouilletm@educ.college-montreal.qc.ca>
2022-08-31 10:40:08 -0400

    count 222

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 # Micaël Brouillet 404
 # word count
-import sys
-# Je met une loupe pour repeter mon programme.
-while True:
-    # Je demande quelle phrase l'utilisateur veut.
-    chaine = input('Écriver une phrase.')
-    # Le programme print le nombre de mot dans la phrase.
-    print("Votre phrase a", len(chaine.split(" ")), "mot(s).")
-    sys.exit(0)
+# Je demande quelle phrase l'utilisateur veut.
+chaine = input('Écriver une phrase.')
+# Le programme print le nombre de mot dans la phrase.
+print("Votre phrase a", len(chaine.split(" ")), "mot(s).")
 
