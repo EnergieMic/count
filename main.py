19c2d19b0ef21698ad03b806298db5ee408d8634
Micael8 <112488116+Micael8@users.noreply.github.com>
2022-09-02 13:32:14 -0400

    Update main.py
    Final iPad

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 # Micaël Brouillet 404
 # word count
+# Je crée une loupe pour reperter le programme.
+while True:
 # Je demande quelle phrase l'utilisateur veut.
-chaine = input('Écriver une phrase.')
+ chaine = input('Écriver une phrase.')
+# Si on ecrit rien et on presse enter le programme se ferme.
+ if not chaine:
+  print("Vous avez quitter le programme.")
+  exit()
 # Le programme print le nombre de mot dans la phrase.
-print("Votre phrase a", len(chaine.split(" ")), "mot(s).")
+ print("Votre phrase a", len(chaine.split(" ")), "mot(s).")
 
